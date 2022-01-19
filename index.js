661adf4a0902d46cb4736f0d782ccdd3b947bb33
ConnorMacleod1 <>
2022-01-19 10:38:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+*Connor Macleod
+*19/01/2021
+*CodeClan Exercise
+*Version 2.
+*/
+
+//Funtion to get country and weather type
+function getWeather(country = 'Scotland', weatherType ='sunny'){
+
+  console.log(`The weather in ${country} is ${weatherType}`)
+  //output
+}
+
+
+// getWeather is called 3 times with different countrys and weather types inplace of the template literals.
+getWeather();
+getWeather("Italy", "rainy")
+getWeather("Canada", "snowing")
