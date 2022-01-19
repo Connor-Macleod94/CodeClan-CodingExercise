2d5771a4df16597f1e3676cca1d0458f46bcc10a
Connor Macleod <>
2022-01-19 10:42:41 +0000

    Version 2. Less lines thus more efficient

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 /*
 *Connor Macleod
-*14/01/2021
+*19/01/2021
 *CodeClan Exercise
-*Version 1.
+*Version 2.
 */
 
-const  getWeather = function (country, weatherType) {
-  return 'The weather in ' + country +' is '+ weatherType +'.';
-} 
-// End of getWeather function.
+//Funtion to get country and weather type
+function getWeather(country = 'Scotland', weatherType ='sunny'){
 
-var getScotlandWeather = getWeather("Scotland", "sunny")
-var getItalyWeather = getWeather("Italy", "rainy")
-var getCanadaWeather = getWeather("Canada", "snowing")
-// variables declaring countries and weathertypes
+  console.log(`The weather in ${country} is ${weatherType}`)
+  //output
+}
 
-console.log("\n",getScotlandWeather,"\n",getItalyWeather,"\n",getCanadaWeather);
-//output "The weather in _____ is ______" with new line entrys to seperate the outputs. x3
-
-
-//end
+// getWeather is called 3 times with different countrys and weather types inplace of the template literals.
+getWeather();
+getWeather("Italy", "rainy")
+getWeather("Canada", "snowing")
